2f5acf90c74c0938cc3b489fc072f6056a742e6f
michaelb2001 <brunco.michael@gmail.com>
2022-03-24 13:53:57 +0100

    api-vue 1

---
+++ resources/js/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="contenitore">
+      <ul>
+          <li v-for="(post,index) in posts" :key="index">
+              <div class="carta">
+                <h3>{{post.title}}</h3>
+                <p>{{post.content}}</p>
+              </div>
+
+              
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data(){
+        return{
+            posts : []
+        }    
+    },
+    created(){
+        axios
+        .get('/api/posts')
+        .then((response)=> {
+    
+        this.posts = response.data;
+  })
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.contenitore{
+    background-image: url(https://www.smartworld.it/images/2021/11/09/windows-11-se-sfondo_1200x675.png);
+    background-position: center center;
+    background-size:cover ;
+    background-repeat: no-repeat;
+    padding: 20px;
+    ul{
+    list-style-type: none;
+
+        li{
+
+        text-decoration: none;
+
+            .carta{
+
+                max-width: 300px;
+                margin: 10px auto;
+                display: flex;
+                flex-direction: column;
+                flex-wrap:wrap;
+                align-content: center;
+                justify-content: center;
+
+                background: rgba( 255, 255, 255, 0.25 );
+                box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+                backdrop-filter: blur( 4px );
+                -webkit-backdrop-filter: blur( 4px );
+                border-radius: 10px;
+                border: 1px solid rgba( 255, 255, 255, 0.18 );
+            }
+        }
+    }
+
+}
+</style>
---
+++ resources/js/front.js
@@ -0,0 +1,20 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+//require('./bootstrap');
+
+window.Vue = require('vue');
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import App from './App.vue';
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
